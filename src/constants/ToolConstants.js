a6b9a88255c72b85016874020a4b47b1e980d49e
kiki le singe <aalbertini95@gmail.com>
2015-09-30 21:21:11 +0200

    refactor(ToolConstants): rename constants

--- src/constants/ToolConstants.js
+++ src/constants/ToolConstants.js
@@ -1,10 +1,8 @@
-// https://github.com/facebook/flux/blob/master/examples/flux-todomvc/js/constants/TodoConstants.js
-
 export default {
-  TOOL_CREATE: 'TOOL_CREATE',
-  TOOLS_FETCH: 'TOOLS_FETCH',
-  TOOL_FETCH: 'TOOL_FETCH',
-  TOOL_UPDATE: 'TOOL_UPDATE',
-  TOOL_DELETE: 'TOOL_DELETE',
-  TOOL_SEARCH: 'TOOL_SEARCH',
+  ADD_TOOL: 'ADD_TOOL',
+  RECEIVE_TOOLS: 'RECEIVE_TOOLS',
+  FETCH_TOOL: 'FETCH_TOOL',
+  UPDATE_TOOL: 'UPDATE_TOOL',
+  DELETE_TOOL: 'DELETE_TOOL',
+  SEARCH_TOOL: 'SEARCH_TOOL',
 };
