c9efb9a7143b92c0f35c9c2ebf5990d7d7b33f0c
kiki le singe <aalbertini95@gmail.com>
2015-09-30 15:11:43 +0200

    fix(index): fix store path

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 
-import configureStore from 'stores';
+import configureStore from 'store';
 
 import AppRouter from './AppRouter';
 // The both Framework7 and Dom7 are exposed in window.*
