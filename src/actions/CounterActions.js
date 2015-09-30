d1f2303dcf7e328083c84fa1d618107e6f43e943
kiki le singe <aalbertini95@gmail.com>
2015-09-30 15:03:32 +0200

    feat(CounterActions): add counter action creators

---
+++ src/actions/CounterActions.js
@@ -0,0 +1,18 @@
+// http://rackt.github.io/redux/docs/basics/Actions.html
+// https://github.com/rackt/redux/blob/master/examples/todomvc/actions/todos.js
+
+import * as types from 'constants/CounterConstants';
+
+/*
+ * action creators
+ */
+
+const increment = () => {
+  return { type: types.INCREMENT_COUNTER };
+};
+
+const decrement = () => {
+  return { type: types.DECREMENT_COUNTER };
+};
+
+export { increment, decrement };
