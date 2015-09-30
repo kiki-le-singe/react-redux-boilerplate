5aee938372bcbd7ac9f2675103a4f5a4ff243552
kiki le singe <aalbertini95@gmail.com>
2015-09-30 21:24:40 +0200

    feat(Tools): use Redux for displays tools

--- src/components/pages/Tools/index.jsx
+++ src/components/pages/Tools/index.jsx
@@ -3,6 +3,10 @@
 // - http://www.idangero.us/framework7/docs/modal.html#indicator
 
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as ToolActions from 'actions/ToolActions';
 
 import Page from 'components/pages/Page';
 import ListSwipeoutTools from 'components/tools/ListSwipeoutTools';
@@ -13,24 +17,37 @@ const defaultProps = {
   isFloatingButtonEnabled: true,
 };
 
+// Which props do we want to inject, given the global state?
+@connect(
+  state => ({
+    tools: state.tools,
+    searchValue: state.searchValue,
+  })
+)
 class Tools extends Page {
 
   // The class' constructor now assumes the role previously filled by componentWillMount
   constructor(props) {
     super(props);
 
+    // Injected by react-redux via connect() call:
+    const { dispatch } = props;
+
+    // bindActionCreators(actionCreators, dispatch): http://rackt.github.io/redux/docs/api/bindActionCreators.html
+    // https://github.com/rackt/redux/blob/master/examples/todomvc/containers/App.js#L11
+    this.actions = bindActionCreators(ToolActions, dispatch);
+
     // Show loader
     f7App.showIndicator();
+  }
 
-    // Warning: getInitialState() is only supported for classes created using React.createClass.
-    // getInitialState() executes exactly once during the lifecycle of the component
-    // and sets up the initial state of the component.
-    // When the state updates, the component re-renders itself.
-    // @see https://facebook.github.io/react/docs/tutorial.html#reactive-state
-    // getInitialState() {
-    //   return {data: []};
-    // }
-    this.state = {tools: [], searchValue: ''};
+  componentDidMount() {
+    super.componentDidMount();
+
+    this.actions.fetchTools().then(() => {
+      // Hide loader
+      f7App.hideIndicator();
+    });
   }
 
   getDataPage() {
@@ -44,13 +61,15 @@ class Tools extends Page {
   }
 
   renderPage() {
+    const { tools, searchValue } = this.props;
+
     return (
       <div className="page-content">
 
         <SearchTools />
 
         <div className="list-block media-list">
-          <ListSwipeoutTools searchValue={this.state.searchValue} data={this.state.tools} />
+          <ListSwipeoutTools searchValue={searchValue} data={tools} />
         </div>
       </div>
     );
