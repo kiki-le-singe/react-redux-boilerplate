c63df55a504b207d552154e1e432daa84242a18f
kiki le singe <aalbertini95@gmail.com>
2015-10-02 17:05:10 +0200

    refactor(tool): cleanup code from tool service

--- src/services/tool.js
+++ src/services/tool.js
@@ -2,69 +2,52 @@ import request from 'superagent';
 
 import api from 'config/api.json';
 
+const end = (resolve, reject) => {
+  return (err, res) => {
+    if (res.ok) {
+      resolve(res.body);
+    } else {
+      console.error(api.tools, res.text); // eslint-disable-line
+      reject(res.text);
+    }
+  };
+};
+
 class Tool {
 
-    static fetch() {
-      return new Promise((resolve, reject) => {
-        request
-           .get(api.tools)
-           .end((err, res) => {
-             if (res.ok) {
-               resolve(res.body);
-             } else {
-               console.error(api.tools, res.text); // eslint-disable-line
-               reject(res.text);
-             }
-           });
-      });
-    }
+  static fetch() {
+    return new Promise((resolve, reject) => {
+      request
+         .get(api.tools)
+         .end(end(resolve, reject));
+    });
+  }
 
-    static fetchOne(id) {
-      return new Promise((resolve, reject) => {
-        request
-           .get(api.tools + '/' + id)
-           .end((err, res) => {
-             if (res.ok) {
-               resolve(res.body);
-             } else {
-               console.error(api.tools, res.text); // eslint-disable-line
-               reject(res.text);
-             }
-           });
-      });
-    }
+  static fetchOne(id) {
+    return new Promise((resolve, reject) => {
+      request
+         .get(api.tools + '/' + id)
+         .end(end(resolve, reject));
+    });
+  }
 
-    static create(tool) {
-      return new Promise((resolve, reject) => {
-        request
-           .post(api.tools)
-           .type('form')
-           .send(tool)
-           .end((err, res) => {
-             if (res.ok) {
-               resolve(res.body);
-             } else {
-               console.error(api.tools, res.text); // eslint-disable-line
-               reject(res.text);
-             }
-           });
-      });
-    }
+  static create(tool) {
+    return new Promise((resolve, reject) => {
+      request
+         .post(api.tools)
+         .type('form')
+         .send(tool)
+         .end(end(resolve, reject));
+    });
+  }
 
-    static delete(id) {
-      return new Promise((resolve, reject) => {
-        request
-           .del(api.tools + '/' + id)
-           .end((err, res) => {
-             if (res.ok) {
-               resolve(res.body);
-             } else {
-               console.error(api.tools, res.text); // eslint-disable-line
-               reject(res.text);
-             }
-           });
-      });
-    }
+  static delete(id) {
+    return new Promise((resolve, reject) => {
+      request
+         .del(api.tools + '/' + id)
+         .end(end(resolve, reject));
+    });
+  }
 }
 
 export default Tool;
