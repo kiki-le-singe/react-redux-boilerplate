f3dcdce41289e5ed2a6b7b2b07056ad48f281a58
kiki le singe <aalbertini95@gmail.com>
2015-10-03 15:00:53 +0200

    feat(IndicatorActions): add indicator actions

---
+++ src/actions/IndicatorActions.js
@@ -0,0 +1,19 @@
+import types from 'constants/IndicatorConstants';
+
+/*
+ * action creators
+ */
+
+function showIndicator() {
+  return {
+    type: types.SHOW_INDICATOR,
+  };
+}
+
+function hideIndicator() {
+  return {
+    type: types.HIDE_INDICATOR,
+  };
+}
+
+export default { showIndicator, hideIndicator };
