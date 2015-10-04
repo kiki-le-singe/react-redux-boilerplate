e61f744e9acfb65b4411dae11e172588db1a8710
kiki le singe <aalbertini95@gmail.com>
2015-10-04 19:43:26 +0200

    refactor(reducers.tools): use spread operator

--- src/reducers/tools.js
+++ src/reducers/tools.js
@@ -9,8 +9,8 @@ const tools = (state = initialState, action) => {
   switch (action.type) {
     case types.RECEIVE_TOOLS:
       return {
+        ...state,
         items: action.tools,
-        searchValue: '',
       };
 
     default:
