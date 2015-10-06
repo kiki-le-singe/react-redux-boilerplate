ee7d95726ca331fa69925e52bf6cdc9ea7766ef3
kiki le singe <aalbertini95@gmail.com>
2015-10-06 20:02:14 +0200

    refactor(Tool): implement Redux

--- src/components/pages/Tools/Tool.jsx
+++ src/components/pages/Tools/Tool.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
 
 import TopNavBar from 'components/TopNavBar';
 import Page from 'components/pages/Page';
@@ -8,22 +9,12 @@ const defaultProps = {
   isPageCached: true,
 };
 
+@connect(
+  state => ({
+    tools: state.tools,
+  })
+)
 class Tool extends Page {
-
-  // The class' constructor now assumes the role previously filled by componentWillMount
-  constructor(props) {
-    super(props);
-
-    // Warning: getInitialState() is only supported for classes created using React.createClass.
-    // getInitialState() executes exactly once during the lifecycle of the component
-    // and sets up the initial state of the component.
-    // When the state updates, the component re-renders itself.
-    // @see https://facebook.github.io/react/docs/tutorial.html#reactive-state
-    // getInitialState() {
-    //   return {data: []};
-    // }
-    this.state = {tool: {}};
-  }
 
   getDataPage() {
     return 'tool';
@@ -34,18 +25,18 @@ class Tool extends Page {
   }
 
   renderPage() {
-    const { title, text, route, slug } = this.state.tool;
-    const iconClassName = classnames('icon', ['icon-', slug].join(''));
+    const { tools: {item} } = this.props;
+    const iconClassName = classnames('icon', ['icon-', item.slug].join(''));
 
     return (
       <div className="page-content">
         <div className="card">
-          <div className="card-header"><i className={iconClassName}></i>{title}</div>
+          <div className="card-header"><i className={iconClassName}></i>{item.title}</div>
           <div className="card-content">
-            <div className="card-content-inner">{text}</div>
+            <div className="card-content-inner">{item.text}</div>
           </div>
           <div className="card-footer">
-            <a href={route} target="_blank">Read more</a>
+            <a href={item.route} target="_blank">Read more</a>
           </div>
         </div>
       </div>
