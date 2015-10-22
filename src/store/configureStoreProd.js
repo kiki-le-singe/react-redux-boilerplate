d70262686f36033d38ff5e53694c77de2db7f336
kiki le singe <aalbertini95@gmail.com>
2015-10-23 01:27:19 +0200

    feat(configureStoreProd): add prod store

---
+++ src/store/configureStoreProd.js
@@ -0,0 +1,20 @@
+// http://rackt.github.io/redux/docs/basics/Store.html
+// https://github.com/rackt/redux/blob/master/examples/todomvc/store/configureStore.js
+
+import { createStore, applyMiddleware, compose } from 'redux';
+
+import logger from 'middleware/logger';
+import toolAPI from 'middleware/toolAPI';
+import rootReducer from 'reducers';
+
+const finalCreateStore = compose(
+  // applyMiddleware(...middlewares): http://rackt.github.io/redux/docs/api/applyMiddleware.html
+  // createStore(reducer, [initialState]): http://rackt.github.io/redux/docs/api/createStore.html
+  applyMiddleware(logger, toolAPI)
+)(createStore);
+
+const configureStoreProd = (initialState) => {
+  return finalCreateStore(rootReducer, initialState);
+};
+
+export default configureStoreProd;
