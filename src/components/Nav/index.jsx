3c49ab191ad7eda672890a36d214b5c32c55d6cb
kiki le singe <aalbertini95@gmail.com>
2015-11-29 21:29:31 +0100

    refactor(Nav): use nav config file

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,19 +2,13 @@ import React from 'react';
 
 import List from 'components/lists/List';
 
+import nav from 'config/nav.json';
+
 class Nav extends React.Component {
   render() {
-    const menuItems = [
-      { route: '/', text: 'Home' },
-      { route: '/hello', text: 'Hello' },
-      { route: '/about', text: 'About' },
-      { route: '/counter', text: 'Counter' },
-      { route: '/tools', text: 'Tools' },
-    ];
-
     return (
       <nav>
-        <List menuItems={menuItems} />
+        <List menuItems={nav} />
       </nav>
     );
   }
