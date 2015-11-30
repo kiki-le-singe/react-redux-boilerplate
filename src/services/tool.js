0388e1ca82f81477278027a820656ff36c4bd37a
kiki le singe <aalbertini95@gmail.com>
2015-11-30 18:36:33 +0100

    refactor(services.tool): reduce/remove useless code

--- src/services/tool.js
+++ src/services/tool.js
@@ -2,52 +2,21 @@ import request from 'superagent';
 
 import api from 'config/api.json';
 
-const end = (resolve, reject) => {
-  return (err, res) => {
-    if (res.ok) {
-      resolve(res.body);
-    } else {
-      console.error(api.tools, res.text); // eslint-disable-line
-      reject(res.text);
-    }
-  };
-};
+function fetch(id = null) {
+  const endpoint = id ? `${api.tools}/${id}` : api.tools;
 
-class Tool {
-
-  static fetch() {
-    return new Promise((resolve, reject) => {
-      request
-         .get(api.tools)
-         .end(end(resolve, reject));
-    });
-  }
-
-  static fetchOne(id) {
-    return new Promise((resolve, reject) => {
-      request
-         .get(`${api.tools}/${id}`)
-         .end(end(resolve, reject));
-    });
-  }
-
-  static create(tool) {
-    return new Promise((resolve, reject) => {
-      request
-         .post(api.tools)
-         .type('form')
-         .send(tool)
-         .end(end(resolve, reject));
-    });
-  }
-
-  static delete(id) {
-    return new Promise((resolve, reject) => {
-      request
-         .del(`${api.tools}/${id}`)
-         .end(end(resolve, reject));
-    });
-  }
+  return request.get(endpoint);
 }
 
-export default Tool;
+function create(tool) {
+  return request
+    .post(api.tools)
+    .type('form')
+    .send(tool);
+}
+
+function del(id) {
+  return request.del(`${api.tools}/${id}`);
+}
+
+export { fetch, create, del };
