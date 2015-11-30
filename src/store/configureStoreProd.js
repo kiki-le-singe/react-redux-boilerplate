f1d57843322c4c2a0c04fb0cb88e1f68548196d3
kiki le singe <aalbertini95@gmail.com>
2015-11-30 18:35:20 +0100

    refactor(middleware.api): implement a better way to call API

--- src/store/configureStoreProd.js
+++ src/store/configureStoreProd.js
@@ -3,13 +3,13 @@
 
 import { createStore, applyMiddleware, compose } from 'redux';
 
-import toolAPI from 'middleware/toolAPI';
+import api from 'middleware/api';
 import rootReducer from 'reducers';
 
 const finalCreateStore = compose(
   // applyMiddleware(...middlewares): http://rackt.github.io/redux/docs/api/applyMiddleware.html
   // createStore(reducer, [initialState]): http://rackt.github.io/redux/docs/api/createStore.html
-  applyMiddleware(toolAPI)
+  applyMiddleware(api)
 )(createStore);
 
 const configureStoreProd = (initialState) => {
