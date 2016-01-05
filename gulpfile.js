9de2bce494758c45b96d1f7cff4fa353ceb06233
kiki le singe <aalbertini95@gmail.com>
2016-01-05 21:46:06 +0100

    refactor(gulpfile): use es6 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@
 
 // https://github.com/gulpjs/gulp/blob/master/docs/recipes/split-tasks-across-multiple-files.md
 // http://makina-corpus.com/blog/metier/2015/make-your-gulp-modular
-var requireDir = require('require-dir');
+const requireDir = require('require-dir');
 
-requireDir('./gulp/tasks', {recurse: true});
+requireDir('./gulp/tasks', { recurse: true });
