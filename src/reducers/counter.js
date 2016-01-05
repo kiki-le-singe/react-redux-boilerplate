7e5185d2158c439323173ae80906b029fd136ff9
kiki le singe <aalbertini95@gmail.com>
2016-01-05 01:09:47 +0100

    fix(counter): fix constants import

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,14 +1,14 @@
 // http://rackt.github.io/redux/docs/basics/Reducers.html
 // https://github.com/rackt/redux/blob/master/examples/todomvc/reducers/todos.js
 
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from 'constants/CounterConstants';
+import types from 'constants/CounterConstants';
 
 const counter = (state = 0, action) => {
   switch (action.type) {
-    case INCREMENT_COUNTER:
+    case types.INCREMENT_COUNTER:
       return state + 1;
 
-    case DECREMENT_COUNTER:
+    case types.DECREMENT_COUNTER:
       return state - 1;
 
     default:
