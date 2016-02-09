0500f8c75935bb21258ed58731e0d07c8e850bc1
kiki le singe <aalbertini95@gmail.com>
2016-02-09 15:35:42 +0100

    feat(routes): add NotFound route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@
   // - https://github.com/rackt/react-router/blob/master/docs/guides/basics/Histories.md
 
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, Redirect } from 'react-router';
 
 import App from 'containers/App';
 import Home from 'components/pages/Home';
@@ -13,6 +13,7 @@ import About from 'components/pages/About';
 import Hello from 'components/pages/Hello';
 import Counter from 'components/pages/Counter';
 import Tools from 'components/pages/Tools';
+import NotFound from 'components/pages/NotFound';
 
 export default (
   <Route path="/" component={App}>
@@ -22,7 +23,7 @@ export default (
     <Route path="about" component={About} />
     <Route path="counter" component={Counter} />
     <Route path="tools" component={Tools} />
-    { /* https://github.com/rackt/react-router/blob/master/UPGRADE_GUIDE.md#notfound-route */ }
-    <Route path="*" component={Home}/>
+    <Route path="404" component={NotFound}/>
+    <Redirect from="*" to="404" />
   </Route>
 );
