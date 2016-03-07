05fcedcb8a1c851d07c1ef041db1c3803613924b
kiki le singe <aalbertini95@gmail.com>
2016-03-07 23:34:01 +0100

    style(AppLayout): remove unused `Component instance

--- src/layouts/AppLayout/index.jsx
+++ src/layouts/AppLayout/index.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 
 import LeftSidePanel from 'components/LeftSidePanel';
 import RightSidePanel from 'components/RightSidePanel';
