e67f6394ae61ebf596c5e76bc0428bdc97ec1ab8
kiki le singe <aalbertini95@gmail.com>
2016-03-22 12:16:03 +0100

    feat(Menu): toggle menu on click

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import Slideout from 'slideout';
 
-export default function Menu() {
+const contextTypes = {
+  slideout: PropTypes.instanceOf(Slideout),
+};
+
+function Menu(props, context) {
+  const toggleMenu = () => {
+    context.slideout.toggle();
+  };
+
   return (
     <nav id="menu">
       <h2>Menu</h2>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/hello">Hello</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/tools">Tools</Link></li>
-        <li><Link to="/counter">Counter</Link></li>
+        <li><Link to="/" onClick={ toggleMenu }>Home</Link></li>
+        <li><Link to="/hello" onClick={ toggleMenu }>Hello</Link></li>
+        <li><Link to="/about" onClick={ toggleMenu }>About</Link></li>
+        <li><Link to="/tools" onClick={ toggleMenu }>Tools</Link></li>
+        <li><Link to="/counter" onClick={ toggleMenu }>Counter</Link></li>
       </ul>
     </nav>
   );
 }
+
+Menu.contextTypes = contextTypes;
+
+export default Menu;
