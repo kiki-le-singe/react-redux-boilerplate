31e11c49e6c5aa449320ea36ac9561934a15716b
kiki le singe <aalbertini95@gmail.com>
2016-03-22 12:12:10 +0100

    feat(AppLayout): implement Slideout

--- src/layouts/AppLayout/index.jsx
+++ src/layouts/AppLayout/index.jsx
@@ -1,17 +1,55 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import Slideout from 'slideout';
+
+import Menu from 'components/Menu';
+import Main from 'components/Main';
 
 const propTypes = {
   children: PropTypes.object,
 };
+const childContextTypes = {
+  slideout: PropTypes.instanceOf(Slideout),
+};
 
-function AppLayout(props) {
-  return (
-    <div className="app-wrapper">
-      { props.children }
-    </div>
-  );
+class AppLayout extends Component {
+
+  constructor() {
+    super();
+
+    this.state = { slideout: null };
+  }
+
+  getChildContext() {
+    return { slideout: this.state.slideout };
+  }
+
+  componentDidMount() {
+    const slideout = new Slideout({
+      panel: document.getElementById('main'),
+      menu: document.getElementById('menu'),
+      padding: 256,
+      tolerance: 70,
+    });
+
+    // Disable eslint because it screams... Why? I don't know because in React
+    // we can use setState() in componentDidMount().
+    // See https://facebook.github.io/react/docs/context.html#updating-context
+    this.setState({ slideout }); // eslint-disable-line
+  }
+
+  render() {
+    return (
+      <div className="views">
+        <Menu />
+        <Main>
+          { this.props.children }
+        </Main>
+      </div>
+    );
+  }
 }
 
 AppLayout.propTypes = propTypes;
+AppLayout.childContextTypes = childContextTypes;
 
 export default AppLayout;
