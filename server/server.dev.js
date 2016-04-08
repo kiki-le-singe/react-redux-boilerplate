5869d87c082911e63b1f0812c134615339c3a887
kiki le singe <aalbertini95@gmail.com>
2016-04-08 21:56:08 +0200

    refactor(logger): Remove implementation of logger

--- server/server.dev.js
+++ server/server.dev.js
@@ -2,7 +2,6 @@ import express from 'express'; // Web framework
 import webpack from 'webpack';
 import path from 'path';
 
-import logger from './logger';
 import routes from './routes';
 import webpackDevMiddleware from './middleware/webpack-dev';
 import webpackHotMiddleware from './middleware/webpack-hot';
@@ -35,5 +34,4 @@ routes(app);
 ***************** */
 
 app.listen(projectConfig.SERVER_PORT, () => {
-  logger.info(`Express server listening on projectConfig.SERVER_PORT ${projectConfig.SERVER_PORT} in ${app.settings.env} node`);
 });
